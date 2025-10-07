44b60f7b0020becd13e76c56e0828bb94f24b634
Bashar Eid <bashar.eid@viam.com>
2025-10-07 13:53:52 -0400

    change company name

--- gen/ts/src/lib/config.ts
+++ gen/ts/src/lib/config.ts
@@ -56,7 +56,7 @@ export function serverURLFromOptions(options: SDKOptions): URL | null {
 export const SDK_METADATA = {
   language: "typescript",
   openapiDocVersion: "0.0.1",
-  sdkVersion: "0.1.7",
+  sdkVersion: "0.1.8",
   genVersion: "2.722.5",
-  userAgent: "speakeasy-sdk/typescript 0.1.7 2.722.5 0.0.1 cycas",
+  userAgent: "speakeasy-sdk/typescript 0.1.8 2.722.5 0.0.1 cycas",
 } as const;
